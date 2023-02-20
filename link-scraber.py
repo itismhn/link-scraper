16d8e2496a1f24da5fef55b383bcafea36c26944
itismhn <itismhn@yahoo.com>
2023-02-20 15:37:23 +0330

    update func input

--- link-scraber.py
+++ link-scraber.py
@@ -1,6 +1,5 @@
 from sys import argv
 import validators
-args = argv[1]
 
 def validate_url(url):
     valid_url=validators.url(url)
@@ -9,10 +8,14 @@ def validate_url(url):
     else:
         print('Please Enter a Valid URL!')
 
-def input_url(url):
-    if len(args) > 1:
-        print('[~] Please Enter in valid format:\n\t<~> python link-scraber.py <url>')
-    else:
-        validate_url(url)
+def input_url():
+    try:
+        #args = argv
+        if len(argv) > 2:
+            print('[~] Please Enter in valid format:\n\t<~> python link-scraber.py <url>')
+        else:
+            validate_url(url)
+    except:
+        pass
         
-input_url(args)
+input_url()
