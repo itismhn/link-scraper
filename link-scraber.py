d39644c051e2b677017f9e58ca62703f1b4584d9
itismhn <itismhn@yahoo.com>
2023-02-20 15:23:29 +0330

    import and define url validator function

--- link-scraber.py
+++ link-scraber.py
@@ -1,11 +1,18 @@
 from sys import argv
-args = argv
+import validators
+args = argv[1]
 
+def validate_url(url):
+    valid_url=validators.url(url)
+    if valid_url:
+        print('URL is ok!')
+    else:
+        print('Please Enter a Valid URL!')
 
 def input_url(url):
     if len(args) > 1:
-    print('[~] Please Enter in valid format\n\t<~> python link-scraber.py <url>')
+        print('[~] Please Enter in valid format:\n\t<~> python link-scraber.py <url>')
     else:
-        pass
+        validate_url(url)
         
 input_url(args)
