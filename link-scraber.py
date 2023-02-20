8f88c4cb61758ee99930e41638ec1d399dbc80d7
itismhn <itismhn@yahoo.com>
2023-02-20 15:17:59 +0330

    define input_url function

---
+++ link-scraber.py
@@ -0,0 +1,11 @@
+from sys import argv
+args = argv
+
+
+def input_url(url):
+    if len(args) > 1:
+    print('[~] Please Enter in valid format\n\t<~> python link-scraber.py <url>')
+    else:
+        pass
+        
+input_url(args)
